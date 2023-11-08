98b02d6fe45f52f5af1dc073adfed3a17c7c3ce6
cantfindalex <150286138+cantfindalex@users.noreply.github.com>
2023-11-08 18:37:13 +0000

    Initial commit

---
+++ ChemistryQuizTest/QuizQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace ChemistryQuizTest
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; }
+        public List<string> Options { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
---
+++ ChemistryQuizTest/QuizService.cs
@@ -0,0 +1,29 @@
+﻿namespace ChemistryQuizTest
+{
+    public class QuizService
+    {
+        private List<QuizQuestion> questions;
+
+        public QuizService()
+        {
+            // Questions
+            questions = new List<QuizQuestion>
+        {
+            new QuizQuestion
+            {
+                Question = "What is the chemical symbol for gold?",
+                Options = new List<string> { "Au", "Ag", "Hg", "Fe" },
+                CorrectAnswer = "Au"
+            },
+            // Add more questions here.
+        };
+        }
+
+        public QuizQuestion GetRandomQuestion()
+        {
+            Random random = new Random();
+            return questions[random.Next(questions.Count)];
+        }
+    }
+
+}
